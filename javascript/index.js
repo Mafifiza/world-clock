3ebb5488da80f6d6b7c80fae477eb9967261255c
Mafifiza <168578665+Mafifiza@users.noreply.github.com>
2025-07-04 09:23:27 +0200

    Injected some JavaScript

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,26 @@
+function updateTime() {
+  // Moscow
+  let moscowElement = document.querySelector("#moscow");
+  let moscowDateElement = moscowElement.querySelector(".date");
+  let moscowTimeElement = moscowElement.querySelector(".time");
+  let moscowTime = moment().tz("Europe/Moscow");
 
+  moscowDateElement.innerHTML = moscowTime.format("Do MMMM YYYY");
+  moscowTimeElement.innerHTML = moscowTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+
+  // Kinshasa
+  let kinshasaElement = document.querySelector("#kinshasa");
+  let kinshasaDateElement = kinshasaElement.querySelector(".date");
+  let kinshasaTimeElement = kinshasaElement.querySelector(".time");
+  let kinshasaTime = moment().tz("Africa/Kinshasa");
+
+  kinshasaDateElement.innerHTML = kinshasaTime.format("Do MMMM YYYY");
+  kinshasaTimeElement.innerHTML = kinshasaTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
